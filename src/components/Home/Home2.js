704b854367d339a8ab33cb47f501f199fd3e4d0c
shivamg5080 <118618976+shivamg5080@users.noreply.github.com>
2024-04-07 02:27:40 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -57,7 +57,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/mihiraj779"
+                  href="https://github.com/shivamg5080"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
