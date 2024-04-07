e5ab5fbdf6b54a4374f04ae763bc6eb84baca955
shivamg5080 <118618976+shivamg5080@users.noreply.github.com>
2024-04-08 03:02:04 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
           <h3>Designed by Shivam Gupta</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} MR</h3>
+          <h3>Copyright © {year} SG</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
